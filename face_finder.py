36b6e674f8af348af00df1fd732354e9812eb587
unknown <hjalmar_thulstrup@hotmail.com>
2019-05-05 20:16:20 +0200

    fixing notebook errors

--- face_finder.py
+++ face_finder.py
@@ -20,7 +20,7 @@ def find_face_eyes_and_show_image(fn):
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-    img = cv2.imread(fn)
+    img = cv2.imread('faces.png')
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray)
 
